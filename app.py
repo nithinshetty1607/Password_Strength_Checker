c7c3c8136ed43dfe90e9b1c72b85646ef689d81a
R Shetty <nithin.r.shetty@accenture.com>
2021-06-16 18:00:58 +0530

    first commit

--- app.py
+++ app.py
@@ -3,16 +3,17 @@ import pandas as pd
 import numpy as np
 from word_divide_char import word_divide_char
 
-word_divide_char = word_divide_char()
+
 
 import pickle
 
 
-def word_divide_char(inputs):
-    characters=[]
-    for i in inputs:
-        characters.append(i)
-    return characters
+# def word_divide_char(inputs):
+#     characters=[]
+#     for i in inputs:
+#         characters.append(i)
+
+#     return characters
 
 # load the model from disk
 
@@ -24,12 +25,14 @@ def home():
 
 @app.route('/StrenghChecker',methods=['POST'])
 def StrenghChecker():
+    
     loaded_model=pickle.load(open('model.pickle', 'rb'))
     loaded_vecorizer=pickle.load(open('vectorizer.pickle', 'rb'))
     password=''
     password=request.form.get("pass")
     print(password)
     X_predict=np.array([password])
+    print(X_predict)
     X_predict=loaded_vecorizer.transform(X_predict)
     y_pred=loaded_model.predict(X_predict)
     if y_pred.item()==0:
@@ -45,4 +48,5 @@ def StrenghChecker():
 
 
 if __name__ == '__main__':
+
 	app.run(debug=True)
--- word_divide_char.py
+++ word_divide_char.py
@@ -1,10 +1,10 @@
-class word_divide_char:
-    
-    def word_divide_char(inputs):
+def word_divide_char(inputs):
         
-        characters=[]
-        for i in inputs:
-            characters.append(i)
-        return characters
+    characters=[]
+    for i in inputs:
+        characters.append(i)
+    return characters
+    
+
 
 #load the model from disk
